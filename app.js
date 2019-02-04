fed4a5186f685e152ed8435e0cf10e7667d7fe31
Hirantha <sahanhirantha@gmail.com>
2019-02-04 21:04:20 +0530

    int

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require('express')
+var app = express()
 
-var app = express();
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-app.get('/',function(req,res){
-    res.send("sex you");
-});
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
